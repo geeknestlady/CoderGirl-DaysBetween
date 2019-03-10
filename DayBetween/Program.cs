223445ada4041d3b44f510e99d6840ea6b912dba
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-03-10 14:41:40 -0500

    Test error issues

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,8 +7,25 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
-
+            Console.WriteLine("What is the first date?");
+            string firstDateInput = Console.ReadLine();
+            
+            Console.WriteLine("What is the second date?");
+            string secondDateInput = Console.ReadLine(); 
+            
+            string countdown = DaysBetween(firstDateInput, secondDateInput);
+            Console.WriteLine(countdown);
+           
             Console.ReadLine();
+        }
+        public static string DaysBetween (string firstDateInput, string secondDateInput)
+        {
+            DateTime firstDate = DateTime.Parse(firstDateInput);
+            DateTime secondDate = DateTime.Parse(secondDateInput);
+            TimeSpan countdown = firstDate.Subtract(secondDate);
+            string daysBetween = countdown.TotalDays.ToString();
+
+            return daysBetween;
         }
 
         // TODO: Create that has a method that counts the number of days between two different dates.
